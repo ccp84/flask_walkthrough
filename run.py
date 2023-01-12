e1bd4dacb476fecf2f00801685dc2abbc8d60abe
ccp84 <ccp84@icloud.com>
2023-01-12 14:42:20 +0000

    html>add news page

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -8,11 +9,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+    
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/news")
+def news():
+    data = []
+    with open("data/news.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("news.html", news_items=data)
 
 
 @app.route("/contact")
